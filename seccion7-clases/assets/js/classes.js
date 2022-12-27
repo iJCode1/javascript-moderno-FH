155a2f5d47740aa6caa34837b64d0440033ac93f
Joel <joeldome17@gmail.com>
2022-12-27 02:32:14 -0600

    métodos set y get en las classes de JS

--- seccion7-clases/assets/js/classes.js
+++ seccion7-clases/assets/js/classes.js
@@ -5,6 +5,7 @@ class Estudiante {
   nombre = "";
   apellido = "";
   edad = "";
+  comida = "";
 
   // Se declará el método constructor
   // Es el método que se ejecuta en un inicio antes de hacer una nueva instancia
@@ -13,6 +14,24 @@ class Estudiante {
     this.nombre = nombre;
     this.apellido = apellido;
     this.edad = edad;
+  }
+
+  // Métodos setters y getters
+
+  set setNombre(nombre){
+    this.nombre = nombre;
+  }
+
+  get getNombre(){
+    return this.nombre;
+  }
+
+  set setComidaFavorita(comida){
+    this.comida = comida;
+  }
+
+  get getComidaFavorita(){
+    return `La comida favorita de ${this.nombre} es ${this.comida}`
   }
 
   // Creación de métodos (funciones de un objeto)
@@ -40,3 +59,27 @@ pepe.decirEdad();
   Hola mundo, mi nombre es: Pepe
   Tengo 18 años
 */
+
+// Haciendo uso de los métodos set y get de una clase
+julieta.setNombre = "JulietaEditada";
+console.log(julieta); // Estudiante {nombre: 'JulietaEditada', apellido: 'Carmona', edad: 27}
+
+console.log(julieta.getNombre); // JulietaEditada
+
+julieta.setComidaFavorita = "Tacos";
+console.log(julieta); // Estudiante {nombre: 'JulietaEditada', apellido: 'Carmona', edad: 27, comida: 'Tacos'}
+
+console.log(julieta.getComidaFavorita); // La comida favorita de JulietaEditada es Tacos
+
+/*  
+  Algo curioso que pasa con las clases es que en este punto se pueden 
+  "crear" nuevas propiedades como si de un 'set' se tratara. 
+  Pero esa propiedad solo vivira en la instancia que se añadio, en las demás no.
+*/
+
+julieta.correr = true;
+console.log(julieta); // Estudiante {nombre: 'JulietaEditada', apellido: 'Carmona', edad: 27, comida: 'Tacos', correr: true}
+
+pepe.setComidaFavorita = "Papas"
+console.log(pepe); // Estudiante {nombre: 'Pepe', apellido: 'Jimenez', edad: 18, comida: 'Papas'}
+
