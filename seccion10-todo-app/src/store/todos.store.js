9654a4bb78ffd4c3999ca13b1df50bc008c2be78
Joel <joeldome17@gmail.com>
2023-01-16 23:22:14 -0600

    implementación de lógica de métodos del store

--- seccion10-todo-app/src/store/todos.store.js
+++ seccion10-todo-app/src/store/todos.store.js
@@ -24,32 +24,74 @@ const loadStore = () => {
   throw new Error("Not implemented yet");
 };
 
+/**
+ * Recibe el filtro y retorna la lista de todos filtrados
+ * @param {Filters} filter
+ * @returns retorna la lista de todos filtrados
+ */
 const getTodos = (filter = Filters.All) => {
-  throw new Error("Not implemented yet");
+  switch (filter) {
+    case filter === Filters.All:
+      return [...store.todos];
+    case filter === Filters.Completed:
+      return store.todos.filter((todo) => todo.completed);
+    case filter === Filters.Pending:
+      return store.todos.filter((todo) => !todo.completed);
+    default:
+      throw new Error(`Option ${filter} is not valid`);
+  }
 };
 
+/**
+ * Recibe el todo (texto)
+ * @param {String} todo Ej: "Hacer el proyecto de React"
+ */
 const addTodo = (todo) => {
-  throw new Error("Not implemented yet");
+  if (!todo) throw new Error("Todo is required");
+
+  store.todos.push(new Todo(todo));
 };
 
+/**
+ * Recibe el ID del todo que se quiere eliminar
+ * @param {String} todoId
+ */
 const deleteTodo = (todoId) => {
-  throw new Error("Not implemented yet");
+  if (!todoId) throw new Error("todoId is required");
+
+  store.todos = store.todos.filter((todo) => todo.id !== todoId);
 };
 
 const deleteCompleted = () => {
-  throw new Error("Not implemented yet");
+  store.todos = store.todos.filter((todo) => !todo.completed);
 };
 
+/**
+ * Recibe el ID del todo que se quiere cambiar de filtro
+ * @param {String} todoId
+ */
 const toggleTodo = (todoId) => {
-  throw new Error("Not implemented yet");
+  if (!todoId) throw new Error("todoId is required");
+
+  store.todos = store.todos.map((todo) => {
+    if (todo.id === todoId) {
+      todo.completed = !todo.completed;
+    }
+
+    return todo;
+  });
 };
 
+/**
+ * Recibe el filtro para setearlo
+ * @param {Filters} filter
+ */
 const setFilter = (filter = Filters.All) => {
-  throw new Error("Not implemented yet");
+  store.filter = filter;
 };
 
 const getCurrentFilter = () => {
-  throw new Error("Not implemented yet");
+  return store.filter;
 };
 
 export {
